24175d41953a778ca29e05f92733c4cd5e59beb6
TenebrisCRS <uruso.leo@gmail.com>
2023-04-20 15:51:35 +0300

    added portfolio and footer sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,10 @@
 $(function () {
+  $('.portfolio__list').slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: true,
+    infinite: true,
+    // fade: true,
+    centerPadding: 30,
+  })
 })
